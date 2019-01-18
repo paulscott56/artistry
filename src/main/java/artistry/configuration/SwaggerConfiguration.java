ee58c6559ac2c3c16e3b775c70b2b13abbbe4a3c
paul <pscott209@gmail.com>
2019-01-18 15:45:16 +0000

    updated swagger to 2.8.0

--- src/main/java/artistry/configuration/SwaggerConfiguration.java
+++ src/main/java/artistry/configuration/SwaggerConfiguration.java
@@ -1,5 +1,6 @@
 package artistry.configuration;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
@@ -30,27 +32,29 @@ public class SwaggerConfiguration {
 
 	@Bean
 	public Docket artistryApi() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("artistry.controllers"))
-				.paths(PathSelectors.ant("/*"))
-				.build()
+		return new Docket(DocumentationType.SWAGGER_2).pathMapping("/").select()
+				.apis(RequestHandlerSelectors.basePackage("artistry.controllers")).paths(PathSelectors.any()).build()
 				.apiInfo(apiInfo());
 	}
 
 	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("Artistry SAFe Agile Management").description(
-				"SAFe Management made easy!")
-				.license("Apache License Version 2.0")
-				.licenseUrl("https://www.apache.org/licenses/LICENSE-2.0").version("1.0").build();
+		return new ApiInfoBuilder().title("Artistry SAFe Agile Management").description("SAFe Management made easy!")
+				.license("Apache License Version 2.0").licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+				.version("1.0").build();
 	}
+
+	// @Bean
+	// UiConfiguration uiConfig() {
+	// return new UiConfiguration("validatorUrl", // url
+	// "list", // docExpansion => none | list
+	// "alpha", // apiSorter => alpha
+	// "schema", // defaultModelRendering => schema
+	// false, // enableJsonEditor => true | false
+	// true); // showRequestHeaders => true | false
+	// }
 
 	@Bean
 	UiConfiguration uiConfig() {
-		return new UiConfiguration("validatorUrl", // url
-				"list", // docExpansion => none | list
-				"alpha", // apiSorter => alpha
-				"schema", // defaultModelRendering => schema
-				false, // enableJsonEditor => true | false
-				true); // showRequestHeaders => true | false
+		return UiConfigurationBuilder.builder().displayRequestDuration(true).validatorUrl(StringUtils.EMPTY).build();
 	}
 }
