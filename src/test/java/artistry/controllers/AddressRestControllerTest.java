490e61ec9a33d93973aaf6053eb4bf20b1fb1c07
paul <pscott209@gmail.com>
2019-02-24 11:41:36 +0000

    fixed up some javadocs

---
+++ src/test/java/artistry/controllers/AddressRestControllerTest.java
@@ -0,0 +1,20 @@
+package artistry.controllers;
+
+class AddressRestControllerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAll() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllById() {
+    }
+}
