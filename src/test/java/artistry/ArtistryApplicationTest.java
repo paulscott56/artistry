2b14e50ae9d599b208545dac0b21f9eb0c46af37
paulscott56Mobacar <pscott@mobacar.com>
2019-02-26 09:37:39 +0000

    fixes and changes

---
+++ src/test/java/artistry/ArtistryApplicationTest.java
@@ -0,0 +1,12 @@
+package artistry;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArtistryApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
