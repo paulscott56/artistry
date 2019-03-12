0241286c214c43fedc97013478380390b8f33286
paulscott56Mobacar <pscott@mobacar.com>
2019-03-12 12:36:08 +0000

    added test stubs

---
+++ src/test/java/artistry/services/ArtistryCsvReaderTest.java
@@ -0,0 +1,46 @@
+package artistry.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArtistryCsvReaderTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void readCountryByCode() {
+    }
+
+    @Test
+    void readAllCountriesCsv() {
+    }
+
+    @Test
+    void createPlanetEarth() {
+    }
+
+    @Test
+    void readCountryInfoCsv() {
+    }
+
+    @Test
+    void readCity() {
+    }
+
+    @Test
+    void readRolesCsv() {
+    }
+
+    @Test
+    void importPeople() {
+    }
+}
